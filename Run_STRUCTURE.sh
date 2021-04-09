45514a7acde09162802f8b2967d489f6989e48ce
Weihankk <whzhang@webmail.hzau.edu.cn>
2021-04-09 16:31:16 +0800

    Run STRUCTURE

---
+++ Run_STRUCTURE.sh
@@ -0,0 +1,13 @@
+./structure -K 2 -L 120074 -N 497 -i 4D_SNP.structure -o k2 -D 1521502
+
+./structure -K 3 -L 120074 -N 497 -i 4D_SNP.structure -o k3 -D 1521502
+
+./structure -K 4 -L 120074 -N 497 -i 4D_SNP.structure -o k4 -D 1521502
+
+./structure -K 5 -L 120074 -N 497 -i 4D_SNP.structure -o k5 -D 1521502
+
+./structure -K 6 -L 120074 -N 497 -i 4D_SNP.structure -o k6 -D 1521502
+
+./structure -K 7 -L 120074 -N 497 -i 4D_SNP.structure -o k7 -D 1521502
+
+./structure -K 8 -L 120074 -N 497 -i 4D_SNP.structure -o k8 -D 1521502
